8d3a10e85fd3bd378abc52da9f12d50eeb96a859
cgmaniac-dev <yongamamkolokotho@outlook.com>
2022-11-15 09:21:33 +0200

    Added cell class and its tests.
    Signed-off-by: cgmaniac-dev <yongamamkolokotho@outlook.com>

---
+++ src/main/java/com/cgmaniac/Cell.java
@@ -0,0 +1,57 @@
+package com.cgmaniac;
+
+public class Cell {
+    private Position position;
+    private int size;
+
+    public Cell(Position position, int size) {
+        this.position = position;
+        this.size = size;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((position == null) ? 0 : position.hashCode());
+        result = prime * result + size;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cell other = (Cell) obj;
+        if (position == null) {
+            if (other.position != null)
+                return false;
+        } else if (!position.equals(other.position))
+            return false;
+        if (size != other.size)
+            return false;
+        return true;
+    }
+    
+    
+}
