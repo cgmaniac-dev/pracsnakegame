6bf849629145b05d0a4f70b75e1062a0420addb8
cgmaniac-dev <yongamamkolokotho@outlook.com>
2022-11-20 09:47:30 +0200

    Added abstract food class getters and setters.
    Signed-off-by: cgmaniac-dev <yongamamkolokotho@outlook.com>

--- src/main/java/com/cgmaniac/GameObjects/AbstractFood.java
+++ src/main/java/com/cgmaniac/GameObjects/AbstractFood.java
@@ -3,4 +3,17 @@ package com.cgmaniac.GameObjects;
 public abstract class AbstractFood extends GameObject{
     private int points;
     private int lifeTime;
+    
+    public int getPoints() {
+        return points;
+    }
+    public void setPoints(int points) {
+        this.points = points;
+    }
+    public int getLifeTime() {
+        return lifeTime;
+    }
+    public void setLifeTime(int lifeTime) {
+        this.lifeTime = lifeTime;
+    }
 }
